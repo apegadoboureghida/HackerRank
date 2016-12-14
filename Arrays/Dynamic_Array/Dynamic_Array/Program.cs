928a6bb70a9005777302621b9528ae4c336840ee
Andrés Pegado Boureghida <Andrs Pegado Boureghida>
2016-12-14 17:28:49 -0600

    First Solutions

---
+++ Arrays/Dynamic_Array/Dynamic_Array/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        string[] args_input = Console.ReadLine().Split(' ');
+        int N = int.Parse(args_input[0]);
+        int Q = int.Parse(args_input[1]);
+        //Creating the seqList.
+        int[][] seqList = new int[N][];
+
+        int lastAns = 0;
+
+        for (int i = 0; i < Q; i++)
+        {
+            int[] queryArgs = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+            int y = queryArgs[2];
+            int x = queryArgs[1];
+            int queue = queryArgs[0];
+            int seqListIndex = (x ^ lastAns) % N;
+
+            //Query 1
+            if (queue == 1)
+            {
+                if (seqList[seqListIndex] == null) {
+                    seqList[seqListIndex] = new int[1];
+                    seqList[seqListIndex][0] = y;
+                }else if(seqList[seqListIndex].Length < N)
+                {
+                    int[] temp = new int[seqList[seqListIndex].Length + 1];
+                    seqList[seqListIndex].CopyTo(temp, 0);
+                    temp[temp.Length - 1] = y;
+                    seqList[seqListIndex] = temp;
+                }
+                //Query 2
+            }
+            else
+            {
+                int pos = y % seqList[seqListIndex].Length;
+                lastAns = seqList[seqListIndex][pos];
+
+                //Print lastAns
+                Console.WriteLine(lastAns);
+            }
+        }
+    }
+}
---
+++ Arrays/Left_Rotation/Left_Rotation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Left_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] queryArgs = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+
+            int[] temp = new int[queryArgs[1]];
+
+            for (int i = 0; i < queryArgs[1]; i++)
+            {
+                temp[i] = array[i];
+            }
+
+            for (int i = queryArgs[1]; i<queryArgs[0]; i++)
+            {
+                array[i - queryArgs[1]] = array[i];
+            }
+
+            for(int i = queryArgs[0]- queryArgs[1]; i< queryArgs[0]; i++)
+            {
+                array[i] = temp[i-(queryArgs[0] - queryArgs[1])];
+            }
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
